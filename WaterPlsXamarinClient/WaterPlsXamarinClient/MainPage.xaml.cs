a05af088680d0ba64f25154be64ac82fa0966c57
Drioux Guidry <driouxgd@yahoo.com>
2018-12-31 10:31:20 -0700

    Initial Commit

--- WaterPlsXamarinClient/WaterPlsXamarinClient/MainPage.xaml.cs
+++ WaterPlsXamarinClient/WaterPlsXamarinClient/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
 using Xamarin.Forms;
 
 namespace WaterPlsXamarinClient
@@ -12,6 +13,36 @@ namespace WaterPlsXamarinClient
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void Button_OnPressed(object sender, EventArgs e)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5177/messageHub")
+                .Build();
+
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+
+
+
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                Console.WriteLine($"{user}: {message}");
+            });
+
+            try
+            {
+                await connection.StartAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
